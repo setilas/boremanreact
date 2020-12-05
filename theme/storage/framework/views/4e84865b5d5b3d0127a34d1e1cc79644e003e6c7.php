6a6d137e8143210606a79c783d99abaad14da8c0
SETILAS <jemsheedth@yahoo.com>
2020-12-05 13:45:37 +0530

    Init

--- theme/storage/framework/views/4e84865b5d5b3d0127a34d1e1cc79644e003e6c7.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer <?php if(url()->current() == route('footer-dark')): ?> footer-dark <?php elseif(url()->current() == route('footer-fixed')): ?> footer-fix <?php endif; ?>">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 footer-copyright text-center">
-              <p class="mb-0">Copyright <?php echo e(date('Y')); ?> © Cuba theme by pixelstrap  </p>
-            </div>
-        </div>
-    </div>
-</footer><?php /**PATH /home/ekta/Documents/GitHub/Cuba/resources/views/layouts/light/footer.blade.php ENDPATH**/ ?>
--- theme/storage/framework/views/ea837cd9fd80adb8a57c046d80575ecc0ab1489b.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 footer-copyright text-center">
-              <p class="mb-0">Copyright <?php echo e(date('Y')); ?> © Cuba theme by pixelstrap  </p>
-            </div>
-        </div>
-    </div>
-</footer><?php /**PATH /home/ekta/Documents/GitHub/Cuba/resources/views/layouts/simple/footer.blade.php ENDPATH**/ ?>
