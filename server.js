a9cd19ed28aa6a453c24a57e8e1057e66a3d148c
prasannaMedia <prasannanaik141@gmail.com>
2020-12-07 19:51:43 +0530

    ENQUIRY ROUTES ADDED

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ dbconnect();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server connected at port no ${PORT}`));
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/user/", require("./routes/api/Enquiry"));
