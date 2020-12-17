852c12df71f7bfabee80ccb0d9c97d3eb1e24136
rayeesvenkulam <rayeesvenkulam@gmail.com>
2020-12-17 09:31:01 +0530

    update adduser

--- client/src/components/dashboard/Admin/Adduser.jsx
+++ client/src/components/dashboard/Admin/Adduser.jsx
@@ -1,117 +1,115 @@
 import React, { Fragment } from "react";
 import { Container, Row, Col, Card, CardHeader, CardBody } from "reactstrap";
+import { Link} from "react-router-dom";
 
 
 const Addview = (props) => {
+
+  
   return (
-    <Fragment>
-      
-      <Container fluid={true}>
-        <Row>
-        <div className="container-contact100">
-          <Col sm="10">
-            <Card>
-              <CardHeader style={{backgroundColor:"#7366ff"}}>
-              <h4>
-                  <strong>Add User</strong>
-                </h4>
-               
-              </CardHeader>
-              <CardBody>
-              <form 
-                >
-                  <div class="form-group row">
-                    <label for="inputName" class="col-sm-5 col-form-label">
-                      Vendor Name
-                    </label>
-                    
-
-                    <div class="col-sm-10">
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="inputName"
-                        placeholder="Enter your Name"
-                       
-                      />
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputName" class="col-sm-5 col-form-label">
-                     Vendor Address
-                    </label>
-                    <div class="col-sm-10">
-                      <input
-                        type="textArea"
-                        class="form-control"
-                        id="inputAddress"
-                        placeholder="Enter your Address"
-                       
-                      />
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputName" class="col-sm-5 col-form-label">
-                     Vendor Phone
-                    </label>
-                    
-                    
-                    <div class="col-sm-10">
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="inputphone"
-                        placeholder="Enter your number"
-                       
-                      />
-                    </div>
-                    </div>
-                    <div class="form-group row">
-                    <label for="inputName" class="col-sm-5 col-form-label">
-                     Vendor Email
-                    </label>
-                    
-                    <div class="col-sm-10">
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="inputemail"
-                        placeholder="Enter your email"
-                        
-                      />
-                  </div>
-                  </div>
-                  <div className="form-group d-flex justify-content-center ">
-                    <button className="btn btn-success" type="submit">
-                      Generate Passcode
-                    </button>
-                    </div>
-                    <div class="form-group row">
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputemail"
-                        placeholder="Enter your Passcode"
-                        
-                      />
-
-                  </div>
+    <div className="container-fluid p-0">
+      <div className="row">
+        <div className="col-12">
+          <div className="login-card">
+            <div>
+              <div>
                 
-                  <div className="form-group d-flex justify-content-center ">
-                    <button className="btn btn-success" type="submit">
+              
+              </div>
+              <div className="login-main">
+                <form
+                  className="theme-form"
+                
+                >
+                  <h4>Add User</h4>
+                  <p>Enter vendor details </p>
+                  <div className="form-group">
+                    <label className="col-form-label pt-0">Vendor Name</label>
+                    <div className="form-row">
+                      <div className="col-6">
+                        <input
+                          className="form-control"
+                          type="text"
+                          required=""
+                          placeholder="First name"
+                          name="firstname"
+                        />
+                      </div>
+                      <div className="col-6">
+                        <input
+                          className="form-control"
+                          type="text"
+                          required=""
+                          placeholder="Last name"
+                          name="lastname"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label className="col-form-label">Vendor Address</label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      required=""
+                      placeholder="vendor address"
+                      name="address"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="col-form-label">Vendor Phone</label>
+                    <input
+                      className="form-control"
+                      type="tel"
+                      required=""
+                      placeholder="mobile number"
+                      name="phone"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="col-form-label">Vendor Email</label>
+                    <input
+                      className="form-control"
+                      type="email"
+                      required=""
+                      placeholder="Test@gmail.com"
+                      name="email"
+                     
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="col-form-label">Password</label>
+                    <input
+                      className="form-control"
+                      type="password"
+                      name="login[password]"
+                      required=""
+                      placeholder="*********"
+                      name="password"
+                      
+                      
+                    />
+                    
+                    <div className="show-hide">
+                    <button type="button" class="btn-primary-outline btn-sm">Generate Password</button>
+                    </div>
+                  </div>
+
+                  <div className="form-group mb-0">
+                    <button className="btn btn-primary btn-block" type="submit">
                       Submit
                     </button>
                   </div>
-                </
-                form>
-              </CardBody>
-            </Card>
-          </Col>
+                  
+                </form>
+              </div>
+            </div>
           </div>
-        </Row>
-      </Container>
-    </Fragment>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
+ 
 export default Addview;
