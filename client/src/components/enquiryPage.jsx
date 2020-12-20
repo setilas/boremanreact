8f1e5ed8718fd27ffbaef1a4eb3fee3818c16ed6
prasannaMedia <prasannanaik141@gmail.com>
2020-12-20 12:22:19 +0530

    updated

--- client/src/components/enquiryPage.jsx
+++ client/src/components/enquiryPage.jsx
@@ -1,26 +1,24 @@
 import React, { Fragment } from "react";
 import Breadcrumb from "../layout/breadcrumb";
 import { Container, Row, Col, Card, CardHeader, CardBody } from "reactstrap";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Enquirypage = (props) => {
-  
-
-
   return (
     <Fragment>
       <Breadcrumb parent="Dashboard" title="Vendor Dashboard" />
       <Container fluid={true}>
-        <Row> 
+        <Row>
           <Col sm="12">
             <Card>
               <CardHeader>
                 <h5>HII user</h5>
-              
               </CardHeader>
               <CardBody>
-              <Link to ="/enquiry">
-                  <button className="btn btn-outline-primary">Add Enquiry</button>
+                <Link to="/enquiry">
+                  <button className="btn btn-outline-primary">
+                    Add Enquiry
+                  </button>
                 </Link>
                 <Link to="/viewenquiry">
                   <button className="btn btn-outline-success ml-3">
@@ -28,21 +26,22 @@ const Enquirypage = (props) => {
                   </button>
                 </Link>
                 <Link>
-                  <button className="btn btn-outline-secondary ml-3">Status</button>
+                  <button className="btn btn-outline-secondary ml-3">
+                    Status
+                  </button>
                 </Link>
                 <Link>
                   <button className="btn btn-outline-danger ml-3">
                     Logout
                   </button>
                 </Link>
-               
               </CardBody>
             </Card>
           </Col>
         </Row>
       </Container>
     </Fragment>
-  )
-}
+  );
+};
 
 export default Enquirypage;
--- routes/api/User/Enquiry.js
+++ routes/api/User/Enquiry.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const auth = require("../../../middleware/auth");
 const router = express.Router();
 const Enquiry = require("../../../models/User/Enquiry");
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { name, location, lat, long, estimate, addstatus } = req.body;
   const enquiryField = {};
 
+  enquiryField.user = req.user.id;
   if (name) enquiryField.name = name;
   if (location) enquiryField.location = location;
   if (lat) enquiryField.lat = lat;
@@ -13,18 +15,19 @@ router.post("/", async (req, res) => {
   if (estimate) enquiryField.estimate = estimate;
   if (addstatus) enquiryField.addstatus = addstatus;
   const enquiry = new Enquiry(enquiryField);
-
   try {
     await enquiry.save();
+    enquiry.counter += 1;
+    console.log(enquiry.counter);
     return res.json({ enquiry });
   } catch (err) {
     return res.status(500).send(err);
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   try {
-    const result = await Enquiry.find();
+    const result = await Enquiry.find({ user: req.params.id });
     return res.json(result);
   } catch (err) {
     console.log(err);
