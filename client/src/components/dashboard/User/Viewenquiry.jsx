751bc850e259388220474b2e8b2cb0cfecfa15a8
prasannaMedia <prasannanaik141@gmail.com>
2020-12-24 18:18:48 +0530

    enquiry data table

--- client/src/components/dashboard/User/Viewenquiry.jsx
+++ client/src/components/dashboard/User/Viewenquiry.jsx
@@ -2,86 +2,59 @@ import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import { enquirybyid } from "../../../action/Enquiry";
 import "../../scss/table.scss";
-import Moment from "react-moment";
+import "jquery/dist/jquery.min.js";
+//Datatable Modules
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import $ from "jquery";
 
-const ViewEnquiry = ({
-  enquirybyid,
-  enquiry,
-  profiles,
-  user,
-  id,
-  userData,
-}) => {
+const ViewEnquiry = ({ enquirybyid, profiles, user }) => {
   useEffect(() => {
     enquirybyid(user._id);
+    $(document).ready(function () {
+      $("#example").DataTable();
+    });
   }, [enquirybyid]);
   const activeEnquiry = profiles.length;
   console.log(activeEnquiry);
   return (
-    <Fragment>
-      <div style={{ height: "600px" }} className="tablebody">
-        <div class="container text-center text-white">
-          <div class="row pt-5">
-            <div class="col-lg-8 mx-auto">
-              <h4 className="strong">View Enquiry</h4>
-            </div>
-          </div>
-        </div>
-        <div class="container py-5">
-          <div class="row">
-            <div class="col-lg-7 mx-auto">
-              <div class="card rounded-0 border-0 shadow tablebox1">
-                <div class="card-body p-5">
-                  <div class="table-responsive">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">Date</th>
-                          <th scope="col">Name</th>
-                          <th scope="col">Location</th>
-                          <th scope="col">GPS(lat)</th>
-                          <th scope="col">GPS(long)</th>
-                          <th scope="col">Status</th>
-                        </tr>{" "}
-                      </thead>
-                      {profiles.length > 0 ? (
-                        profiles.map((profile) => (
-                          <tbody>
-                            <tr>
-                              <td>
-                                {
-                                  <Moment format="YYYY/MM/DD">
-                                    {profile.date}
-                                  </Moment>
-                                }
-                              </td>
-                              <td>{profile.name}</td>
-                              <td>{profile.location}</td>
-                              <td>{profile.lat}</td>
-                              <td>{profile.long}</td>
-                              <td>{profile.addstatus}</td>
-                            </tr>
-                          </tbody>
-                        ))
-                      ) : (
-                        <h1>Nothing</h1>
-                      )}
-                    </table>
-                  </div>
-                  {/* <a
-              class="btn btn-primary rounded-0 btn-block"
-              id="insertRow"
-              href="#"
-            >
-              Add new row
-            </a> */}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="MainDiv">
+      <div
+        class="jumbotron text-center"
+        style={{ background: "cornflowerblue" }}
+      >
+        <h3>View Enquiry</h3>
       </div>
-    </Fragment>
+
+      <div className="container">
+        <table id="example" class="table table-hover table-bordered">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>GPS(lat)</th>
+              <th>GPS(long)</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {profiles.map((profile) => {
+              return (
+                <tr>
+                  <td>{profile._id}</td>
+                  <td>{profile.name}</td>
+                  <td>{profile.location}</td>
+                  <td>{profile.lat}</td>
+                  <td>{profile.long}</td>
+                  <td>{profile.addstatus}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
